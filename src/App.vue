42c98dd311cf56e5968ed3dca774106810063c24
yeeeroo <changyiru0318@gmail.com>
2024-01-02 09:58:51 +0800

    catch up

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,30 @@
 <script>
-import { RouterView } from 'vue-router';
-// import HelloWorld from './components/HelloWorld.vue';
+import { RouterView } from 'vue-router'
+
 export default {
   // 元件宣告
   components: {
-    RouterView
+    RouterView,
   },
 }
 </script>
 
 <template>
+  <!-- 觀察你的設計稿 把共用的部分放在此頁面 -->
   <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/calc">Calc</RouterLink>
-      </nav>
+    <RouterLink to="/">home</RouterLink>
+    <RouterLink to="/about">about</RouterLink>
+    <RouterLink to="/calc">calc</RouterLink>
   </header>
+  <main>
+    <RouterView />
+    <div class="calc">我來自公版</div>
+  </main>
+  <footer>
+  </footer>
 
-  <RouterView />
 </template>
 
 <!-- scoped style只影響這支vue -->
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/CalcView.vue
+++ src/views/CalcView.vue
@@ -1,8 +1,62 @@
-<script setup>
+<script>
+export default {
+  // 放置所有變數的位置
+  data() {
+    return {
+      numA: 0,
+      numB: 0,
+      result: 0,
+    };
+  },
+  // 放置所有function的位置
+  methods: {
+    add() {
+      // 先判斷numA numB 不為空
+      if (this.numA.toString().trim() === '' || this.numB.toString().trim() === '') return;
+      this.result = this.numA + this.numB;
+    },
+  },
+}
 </script>
 
 <template>
-    <div></div>
+  <div class="calc">
+    計算機
+    <div>
+      <label>
+        數字A:
+        <!-- v-model -->
+        <input v-model="numA" type="number">
+      </label>
+    </div>
+    <div>
+      <label>
+        數字B:
+        <input v-model="numB" type="number">
+      </label>
+    </div>
+
+    <div class="btns">
+      <button class="btn" type="button" @click="add()">
+        +
+      </button>
+      <button class="btn" type="button">-</button>
+      <button class="btn" type="button">*</button>
+      <button class="btn" type="button">/</button>
+    </div>
+    <div class="output">
+      輸出結果: {{ result }}
+    </div>
+  </div>
 </template>
 
-<style></style>
+<!-- scoped: 目的是為了不要影響到其他區塊的style -->
+<style scoped>
+  .btns {
+    display: flex;
+    gap: 30px;
+  }
+  .btn {
+    padding: 5px 10px;
+  }
+</style>
