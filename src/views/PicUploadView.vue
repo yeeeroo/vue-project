3c5f3f7c8615f8fa0b10d839bf1816a3ac46e0fe
yeeeroo <changyiru0318@gmail.com>
2024-01-04 15:19:38 +0800

    Update PicUploadView.vue

--- src/views/PicUploadView.vue
+++ src/views/PicUploadView.vue
@@ -1,19 +1,42 @@
 <script>
-export default{
-    data(){
-        return;
+export default {
+    data() {
+        return {
+            picFile: '',
+            filePic: '',
+        }
     },
     methods: {
-
+        uploadPic(event) {
+            // console.log(event);
+            this.picFile = event.target.file[0];
+            console.log(this.picFile);
+            if (this.picFile) {
+                const reader = new FileReader();
+                reader.readAsDataURL(this.picFile);
+                reader.onload = (e) => {
+                    console.log(e);
+                    this.filePic = e.target.result;
+                    this.imageFile = [
+                        {
+                            name: this.picFile.name,
+                            path: this.filePic,
+                        }
+                    ];
+                };
+            }
+        }
     }
 }
 </script>
 
 <template>
     <div>
-
+        <input type="file" accept=".jpg,.png" @change="uploadPic()">
+    </div>
+    <div class="my-3">
+        <button type="button" class="border-2 rounded-lg px-2 py-1">上傳圖片</button>˝
     </div>
 </template>
 
-<style>
-</style>
+<style></style>
