9bb164bc2972b849d24f6237538da88d1edd7a92
yeeeroo <changyiru0318@gmail.com>
2024-01-05 15:45:36 +0800

    Update PicUploadView.vue

--- src/views/PicUploadView.vue
+++ src/views/PicUploadView.vue
@@ -2,40 +2,49 @@
 export default {
     data() {
         return {
-            picFile: '',
+            imageFile: '',
             filePic: '',
+            fileInfo: [],
         }
     },
     methods: {
         uploadPic(event) {
-            // console.log(event);
-            this.picFile = event.target.file[0];
-            console.log(this.picFile);
-            if (this.picFile) {
+            this.imageFile = event.target.files[0];
+            // console.log(this.imageFile);
+            if (this.imageFile) {
                 const reader = new FileReader();
-                reader.readAsDataURL(this.picFile);
+                reader.readAsDataURL(this.imageFile);
                 reader.onload = (e) => {
                     console.log(e);
                     this.filePic = e.target.result;
-                    this.imageFile = [
+                    this.fileInfo = [
                         {
-                            name: this.picFile.name,
+                            name: this.imageFile.name,
                             path: this.filePic,
                         }
+
                     ];
-                };
+                    // console.log(this.fileInfo);
+                }
             }
+        },
+        submit() {
+            this.fileInfo
         }
-    }
-}
+
+    },
+};
 </script>
 
 <template>
-    <div>
-        <input type="file" accept=".jpg,.png" @change="uploadPic()">
+    <div class="my-2">
+        <input type="file" accept=".jpg,.png" @change="uploadPic">
     </div>
-    <div class="my-3">
-        <button type="button" class="border-2 rounded-lg px-2 py-1">上傳圖片</button>˝
+    <div class="w-[500px]">
+        <img :src="filePic" alt="" class="object-cover">
+    </div>
+    <div class="inline-block border-2 rounded-lg px-2 py-1 mt-2">
+        <button type="button" @click="submit()">上傳圖片</button>
     </div>
 </template>
 
